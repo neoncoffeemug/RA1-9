6f897cc7da19dea4395434899ce1088febc2e275
Vinicius Pagani <vini.pagani@hotmail.com>
2026-03-22 20:36:50 -0300

    Salvar tokens .txt

--- src/main.py
+++ src/main.py
@@ -1,19 +1,21 @@
 import sys
 from lerArquivo import lerArquivo
 from parseExpressao import parseExpressao
+from salvarTokens import salvarTokens
 
 if len(sys.argv) < 2:
     print("Uso: python main.py <arquivo>")
     sys.exit(1)
 
 nomeArquivo = sys.argv[1]
-
 linhas = lerArquivo(nomeArquivo)
+
+resultados = []
 
 for linha in linhas:
     tokens = []
     parseExpressao(linha, tokens)
 
-    print("Linha:", linha)
-    print("Tokens:", tokens)
-    print()
+    resultados.append((linha, tokens))  # ← ISSO É IMPORTANTE
+
+salvarTokens("output/tokens.txt", resultados)
