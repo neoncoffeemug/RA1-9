b9f1e367214e02929de4a143eb1869812fd88876
lucas <neoncoffeemug@gmail.com>
2026-03-25 20:14:48 -0300

    Adicão executar expressao e atualização main

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import sys
 from lerArquivo import lerArquivo
 from parseExpressao import parseExpressao
 from salvarTokens import salvarTokens
+from executarExpressao import executarExpressao
 
 if len(sys.argv) < 2:
     print("Uso: python main.py <arquivo>")
@@ -12,15 +13,32 @@ linhas = lerArquivo(nomeArquivo)
 
 resultados = []
 
+memoria = {}
+historico = []
+
 for linha in linhas:
     tokens = []
-
     valido = parseExpressao(linha, tokens)
 
     if not valido:
-        print(f"Erro léxico na linha: {linha}")
-        continue  # ignora linha inválida
+        print(f"Erro léxico: {linha}")
+        continue
 
     resultados.append((linha, tokens))
 
-salvarTokens("../output/tokens.txt", resultados)
+    try:
+        resultado = executarExpressao(tokens, memoria, historico)
+
+        historico.append(resultado)
+
+        print("Linha:", linha)
+        print("Resultado:", resultado)
+        print("Memória:", memoria)
+        print()
+
+    except Exception as e:
+        print(f"Erro na execução: {linha}")
+        print("Motivo:", e)
+        print()
+
+salvarTokens("output/tokens.txt", resultados)
